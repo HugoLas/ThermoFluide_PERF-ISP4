4bcc1446d7357c57393189babb1f5a04f00290c5
Hugo <hugo.lascouts@outlook.fr>
2024-10-14 21:51:08 +0200

    Finalisation auto-selection du nombre de racine, resolution bugs, prise en compte de plus de cas critiques

--- TestPtsDepart.c
+++ TestPtsDepart.c
@@ -1,9 +1,16 @@
 #include <math.h>   // Permet d'effectuer des calculs
 #include <stdlib.h> // Permet l'allocation dynamic de mémoire
 #include <stdio.h>
+#include <stdbool.h> // Permet d'utiliser un booléen. Pas vraiment obligatoire mais plus compréhensible
 
-double * PointsDepartNewton(double borneInf, double borneSup, double ab[]);
+typedef struct{     // Définit une structure "Tableau" qui permet d'obtenir à la fois les données du tableau mais aussi sa taille. (en fonction du nb. de racine, la taille change)
+    int taille;
+    double *donnees;
+} Tableau;
+
+void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double ab[]); // La fonction prend en entrée l'adresse du tableau sur lequel travailler, les bornes de la fonction, ses paramètres.
 double PR(double A, double B, double C, double D, double Z);
+void instancierTableau(Tableau *tab, int taille);
 
 int main(){
     printf("J'entre dans main \n");
@@ -26,46 +33,46 @@ int main(){
 
 
     printf("J'ai peuplé abg \n");
-    double *MonTableau = PointsDepartNewton(0,4,abg); 
 
-    for (boucle = 0; boucle < 6; boucle++) // (ou boucle < 4 pour 2 racines ou boucle < 2 pour une racine)
+    Tableau *TabPointsDepart = (Tableau*)malloc(sizeof(Tableau));
+    instancierTableau(TabPointsDepart,6);
+
+    PointsDepartNewton(TabPointsDepart,0,4,abg); 
+
+    for (boucle = 0; boucle < TabPointsDepart->taille; boucle++) // (ou boucle < 4 pour 2 racines ou boucle < 2 pour une racine)
     {
-        printf("%.3f ",MonTableau[boucle]);
+        printf("%.4f ",TabPointsDepart->donnees[boucle]);
         //printf("\n");
         //printf("%d ",boucle);
     }
+    
+    free(TabPointsDepart->donnees);
+    free(TabPointsDepart);
+
     return 0;
 }
 
-double * PointsDepartNewton(double borneInf, double borneSup, double ab[]){ // L'astérisque indique que la fonction renverra un pointeur. Ce pointeur est une adresse mémoire.
-    double pas[] = {0.1,0.01,0.001,0.0001,0.00001}; // Remplacer le premier pas par un pas très grand (3.9 par ex. si l'intervalle des bornes est de 0 à 4) pour démontrer la fonction de sélection automatique du pas
+void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double ab[]){ // L'astérisque indique que la fonction renverra un pointeur. Ce pointeur est une adresse mémoire.
+    double pas[] = {0.1,0.01,0.001}; // Remplacer le premier pas par un pas très grand (3.9 par ex. si l'intervalle des bornes est de 0 à 4) pour démontrer la fonction de sélection automatique du pas
     double abscisse, ordonnee, ordonneeTampon;
     int compteurIntervalle = 0;
     int compteurPas = 0;
     int i;
-    double *tableauAbscisses = (double*)malloc(6 * sizeof(double)); /*Alloue dans la mémoire un emplacement de 6 fois la taille d'un double, 
-                                                                        pour stocker les 6 abscisses qui encadrent les racines. */
-    if (tableauAbscisses == NULL) {
-        printf("Erreur d'allocation mémoire !\n");
-        return NULL;
-    }
-    else
-    {
-        printf("Allocation réussie \n");
-    }
-    while ((compteurIntervalle<6)&&(compteurPas <5)) //A l'issue de trois changements, compteurIntervalle = 6, donc on s'arrête de chercher d'autres racines. Un fois qu'on a testé la 5e valeur de pas (pas[4]), la boucle incrémente de 1 le compteur et on doit s'arrêter car pas[5] n'existe pas.
+    bool ordonneeTamponZero;
+
+    while ((compteurIntervalle<6)&&(compteurPas <3)) //A l'issue de trois changements, compteurIntervalle = 6, donc on s'arrête de chercher d'autres racines. Un fois qu'on a testé la 3e valeur de pas (pas[2]), la boucle incrémente de 1 le compteur et on doit s'arrêter car pas[3] n'existe pas.
     {
         compteurIntervalle = 0; /*  Doit absolument être reset à chaque tour de boucle : Autrement on peut avoir un cas où on trouve un changement de signe au premier tour qui englobe 3 racines, puis avec un pas plus fin 
-                                    on trouve un changement de signe qui englobe seulement les 2 premières + un changement pour la troisième racine 
+                                    on trouve un changement de signe qui englobe seulement les 2 premières + un changement de signe pour la troisième racine 
                                     -> on aura saturé le tableau sans avoir trois intervalles sans intersection. Donc on reset la progression et on recommence à écrire
                                     au début du tableau quand on change de pas. */ 
         abscisse = borneInf;
         ordonnee = PR(ab[0],ab[1],ab[2],ab[3],abscisse);    // Initialisation de l'ordonnee
         printf("ordonnée = %f\n",ordonnee);
-        if (ordonnee == 0){                      // Si jamais (quasi impossible) on tombe sur zero tout pile avec la premiere abscisse, on a déjà une première racine. 
-            tableauAbscisses[compteurIntervalle] = abscisse - pas[compteurPas];   // Donc pour que la racine soit dans l'intervalle, on retire 1*pas ici..
+        if (ordonnee < 0.000000001 && ordonnee > -0.000000001){                      // Si jamais (quasi impossible) on tombe sur zero tout pile avec la premiere abscisse, on a déjà une première racine. 
+            tab->donnees[compteurIntervalle] = abscisse - pas[compteurPas];   // Donc pour que la racine soit dans l'intervalle, on retire 1*pas ici..
             compteurIntervalle = compteurIntervalle +1;
-            tableauAbscisses[compteurIntervalle] = abscisse + pas[compteurPas];   // et on ajoute 1*pas là.
+            tab->donnees[compteurIntervalle] = abscisse + pas[compteurPas];   // et on ajoute 1*pas là.
             compteurIntervalle = compteurIntervalle +1;
         }
         abscisse = abscisse + pas[compteurPas];
@@ -73,20 +80,22 @@ double * PointsDepartNewton(double borneInf, double borneSup, double ab[]){ // L
                                                                             Abscisse + pas au cas où le dernier changement de signe soit sur la borneSup */
         ordonneeTampon = PR(ab[0],ab[1],ab[2],ab[3],abscisse);
         
-        if (ordonneeTampon<0.000000001 && ordonneeTampon >-0.000000001) // Autrement dit, =0 (donc on a trouvé une racine) mais avec les doubles un == pourrait poser problème. Normalement on a environ droit à 15 chiffres significatifs.
+        if (ordonneeTampon<0.000000001 && ordonneeTampon >-0.000000001 && tab->donnees[compteurIntervalle-1]!=abscisse && tab->donnees[compteurIntervalle-1]!=abscisse-pas[compteurPas]) /* Autrement dit, =0 (donc on a trouvé une racine) mais avec les doubles, un == pourrait poser problème. Normalement on a environ droit à 15 chiffres significatifs.
+                                                                                                                                                                                            Je vérifie aussi qu'on vient pas de déclarer l'abscisse précédente (ou actuelle car c'est possible aussi) comme racine, sinon c'est juste qu'on la détecte en double à tort.*/
         {
             printf("ordonneeTampon ~= 0 --> Abscisse : %f, ordonnee : %.10f, ordonneeTampon : %.10f \n",abscisse,ordonnee,ordonneeTampon);
-            tableauAbscisses[compteurIntervalle] = abscisse - pas[compteurPas];
+            tab->donnees[compteurIntervalle] = abscisse - pas[compteurPas];
             compteurIntervalle = compteurIntervalle +1;
-            tableauAbscisses[compteurIntervalle] = abscisse + pas[compteurPas]; /* En réalité si ordonneeTampon vaut 0 c'est qu'on a trouvé une racine. Pour l'exercice je renvoie un intervalle 
+            tab->donnees[compteurIntervalle] = abscisse + pas[compteurPas]; /* En réalité si ordonneeTampon vaut 0 c'est qu'on a trouvé une racine. Pour l'exercice je renvoie un intervalle 
                                                                     qui la contient en ajoutant le pas mais on pourrait directement la renvoyer en tant que telle. */
             compteurIntervalle = compteurIntervalle +1;
+            ordonneeTamponZero = true;
         }
-        else if (ordonneeTampon*ordonnee<0) {
+        else if (ordonneeTampon*ordonnee<0 && tab->donnees[compteurIntervalle-1]!=abscisse && tab->donnees[compteurIntervalle-1]!=abscisse-pas[compteurPas]) {
             printf("ordonneeTampon*ordonnee<0 --> Abscisse : %f, ordonnee : %f, ordonneeTampon : %f \n",abscisse,ordonnee,ordonneeTampon);
-            tableauAbscisses[compteurIntervalle] = abscisse - pas[compteurPas];
+            tab->donnees[compteurIntervalle] = abscisse - pas[compteurPas];
             compteurIntervalle = compteurIntervalle +1;
-            tableauAbscisses[compteurIntervalle] = abscisse;
+            tab->donnees[compteurIntervalle] = abscisse;
             compteurIntervalle = compteurIntervalle +1;
         }
         ordonnee = ordonneeTampon;
@@ -97,19 +106,30 @@ double * PointsDepartNewton(double borneInf, double borneSup, double ab[]){ // L
     
     if (compteurIntervalle != 6) //On part de 0, si on trouve un changement on incrémente 2 fois de 1. Donc compteurIntervalle = 2 avec les cases 0 et 1 remplies. Même raisonnement jusqu'à 6.
     {
-        double *NouvTableauAbscisses = (double*)realloc(tableauAbscisses,compteurIntervalle * sizeof(double)); // Donc si on a pas trouvé 3 racines, on renvoie un tableau qui contient le bon nombre de racines seulement.
+        double *NouvTableauAbscisses = (double*)realloc(tab->donnees,compteurIntervalle * sizeof(double)); // Donc si on a pas trouvé 3 racines, on renvoie un tableau qui contient le bon nombre de racines seulement.
         if (NouvTableauAbscisses != NULL) 
         {
-            tableauAbscisses = NouvTableauAbscisses; //Pas besoin de free quoique ce soit. Realloc a déjà libéré l'ancienne mémoire. Je crois que NouvTableauAbscisse demeure valable et est indispendable. En revanche, il n'y a bien qu'un seul espace mémoire d'alloué.
+            tab->donnees = NouvTableauAbscisses; //Pas besoin de free quoique ce soit. Realloc a déjà libéré l'ancienne mémoire. Je crois que NouvTableauAbscisse demeure valable et est indispendable. En revanche, il n'y a bien qu'un seul espace mémoire d'alloué.
+            tab->taille = compteurIntervalle;
+            printf("Reallocation de la mémoire\n");
         }
         else{
-            printf("Erreur de réallocation mémoire : tableau de 6 cases --> tableau de %d cases",compteurIntervalle);
+            printf("Erreur de réallocation mémoire : tableau de 6 cases --> tableau de %d cases\n",compteurIntervalle);
         }
     }       
-    return tableauAbscisses;
 }
 
 double PR(double A, double B, double C, double D, double Z){
     double Valeur = A*Z*Z*Z+B*Z*Z+C*Z+D;
     return Valeur;
+}
+
+void instancierTableau(Tableau *tab, int taille){   // Cette fonction permet la création du tableau, associée à sa taille. Le tableau est alloué mais non peuplé.
+    tab->donnees = (double*)malloc(taille*sizeof(double));
+    tab->taille = taille;
+    if (tab->donnees == NULL)
+    {
+        printf("Erreur de malloc : Structure tableau, allocation échouée");
+        tab->taille = 0;
+    }
 }
