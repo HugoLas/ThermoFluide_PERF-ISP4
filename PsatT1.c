059f8d610cac2cf266ed84875cf7f660ad7e3133
Hugo <hugo.lascouts@outlook.fr>
2024-10-15 22:26:49 +0200

    Passage en variables global des variables type A, B, Q,... Implémentation cas critique zero racine sur intervalle. Ajout Newton Raphson (et fonctions support) a tester.

--- PsatT1.c
+++ PsatT1.c
@@ -16,31 +16,41 @@ typedef struct{     // Définit une structure "Tableau" qui permet d'obtenir à 
     double *donnees;
 } Tableau;
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~ Déclaration des variables ~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~ Déclaration des variables globales ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /* Ces variables appartiennent à l'espace de travail global. Elles existent durant la durée de vie du programme.
-Par opposition, les variables locales (celles déclarées dans les fonctions) ne vivent que pendant l'execution de la fonction.*/
+Par opposition, les variables locales (celles déclarées dans les fonctions) ne vivent que pendant l'execution de la fonction.
+Toute sous fonction peut manipuler les variables globales. */
 
-float T1;
-float Tc;
-float Pc;
-float Omega_Ag; //Float a une précision de 7 chiffres, Omega fournit a 6 chiffres significatifs.
-float Omega_Bg;
-double Trg;
-double Prg;
-float acentric;
+double T1;
+double Tc;
+double Pc;
+double Omega_A; 
+double Omega_B;
+double Tr;
+double Pr;
+double acentric;
+double alpha;
+double A;
+double B;
+double Q;
 double ab[2]; // Contiendra les paramètres A et B pour Peng Robinson
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ Déclarations des fonctions ~~~~~~~~~~~~~~~~~~~~~~~~~
 
 /* Déclare les fonctions avant main pour les signaler au compiler. */
 
-double trouveA(float Omega_A, float acentric, double Tr, double Pr);
-double trouveB(float Omega_B, double Tr, double Pr);
-double trouveAlpha(float acentric, double Tr);
-double PR(double A, double B, double Z);
-double derivePR(double A, double B, double Z);
-void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double ab[]); // La fonction prend en entrée l'adresse du tableau sur lequel travailler, les bornes de la fonction, ses paramètres.
+double trouveA(double Pr);
+double trouveB(double Pr);
+double trouveAlpha(double acentric);
+double PR(double Z);
+double derivePR(double Z);
+double trouveQZB(double Z);
+double trouvePhi(double Z);
+double valeurMax(Tableau *tab);
+double valeurMin(Tableau *tab);
+void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup); // La fonction prend en entrée l'adresse du tableau sur lequel travailler, les bornes de la fonction, ses paramètres.
+void NewtonRaphson(Tableau *tabResult, Tableau *tabIntervalles);
 void instancierTableau(Tableau *tab, int taille);
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~ Programme principal ~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -49,17 +59,20 @@ void instancierTableau(Tableau *tab, int taille);
 
 int main(){
 printf("Valeur de T1 : \n");
-scanf("%f", &T1); //%f spécifie que l'on attend un float, & spécifie à quelle variable attribuer la valeur.
+scanf("%lf", &T1); //%lf spécifie que l'on attend un double, & spécifie à quelle variable attribuer la valeur.
 printf("Valeur de Tc : \n");
-scanf("%f", &Tc);
+scanf("%lf", &Tc);
 printf("Valeur de Pc : \n");
-scanf("%f", &Pc);
+scanf("%lf", &Pc);
 printf("Valeur de Omega_A : \n");
-scanf("%f", &Omega_Ag);
+scanf("%lf", &Omega_A);
 printf("Valeur de Omega_B : \n");
-scanf("%f", &Omega_Bg);
+scanf("%lf", &Omega_B);
+printf("Valeur du facteur acentric w : \n");
+scanf("%lf", &acentric);
 
-Trg = T1/Tc;
+alpha = trouveAlpha(acentric);
+Tr = T1/Tc;
 
 return 0;
 };  
@@ -70,33 +83,33 @@ return 0;
 /* Ici on donne le contenu des fonctions dont on aura besoin. Ces fonctions peuvent être appelées par le main
 tout commme elles peuvent s'appeler entre elles. */
 
-double trouveA(float Omega_A, float acentric, double Tr, double Pr){
-    double A = Omega_A*trouveAlpha(acentric,Tr)*(Pr/(Tr*Tr));
+double trouveA(double Pr){
+    double A = Omega_A*alpha*(Pr/(Tr*Tr));
     return A;
 }
 
-double trouveB(float Omega_B, double Tr, double Pr){
+double trouveB(double Pr){
     double B = Omega_B*(Pr/Tr);
     return B;
 }
 
-double trouveAlpha(float acentric, double Tr){
+double trouveAlpha(double acentric){
     double m = 0.37464+(1.54226*acentric)-(0.26992*acentric*acentric);
     double alpha = (1+m*(1-sqrt(Tr)))*(1+m*(1-sqrt(Tr)));
     return alpha;
 }
 
-double PR(double A, double B, double Z){
+double PR(double Z){
     double Valeur = Z*Z*Z+(B-1)*Z*Z+(A-2*B-3*B*B)*Z-B*(A-B-B*B);
     return Valeur;
 }
 
-double derivePR(double A, double B, double Z){
+double derivePR(double Z){
     double derive = 3*Z*Z+2*(B-1)*Z+(A-2*B-3*B*B);
     return derive;
 }
 
-void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double ab[]){ // L'astérisque indique que la fonction renverra un pointeur. Ce pointeur est une adresse mémoire.
+void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup){ // L'astérisque indique que la fonction renverra un pointeur. Ce pointeur est une adresse mémoire.
     double pas[] = {0.1,0.01,0.001}; // Remplacer le premier pas par un pas très grand (3.9 par ex. si l'intervalle des bornes est de 0 à 4) pour démontrer la fonction de sélection automatique du pas
     double abscisse, ordonnee, ordonneeTampon;
     int compteurIntervalle = 0;
@@ -110,7 +123,7 @@ void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double a
                                     -> on aura saturé le tableau sans avoir trois intervalles sans intersection. Donc on reset la progression et on recommence à écrire
                                     au début du tableau quand on change de pas. */ 
         abscisse = borneInf;
-        ordonnee = PR(ab[0],ab[1],abscisse);    // Initialisation de l'ordonnee
+        ordonnee = PR(abscisse);    // Initialisation de l'ordonnee
         printf("ordonnée = %f\n",ordonnee);
         if (ordonnee < 0.000000001 && ordonnee > -0.000000001){                      // Si jamais (quasi impossible) on tombe sur zero tout pile avec la premiere abscisse, on a déjà une première racine. 
             tab->donnees[compteurIntervalle] = abscisse - pas[compteurPas];   // Donc pour que la racine soit dans l'intervalle, on retire 1*pas ici..
@@ -121,7 +134,7 @@ void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double a
         abscisse = abscisse + pas[compteurPas];
         while ((abscisse <= borneSup + pas[compteurPas]) && (compteurIntervalle < 5)) {  /* A l'issue du 2e changement, compteurIntervalle vaut 4, a l'issue du 3e changement, il vaut 6. (je crois que je pouvais mettre compteurIntervalle < 6 mais ca marche comme ca) 
                                                                             Abscisse + pas au cas où le dernier changement de signe soit sur la borneSup */
-        ordonneeTampon = PR(ab[0],ab[1],abscisse);
+        ordonneeTampon = PR(abscisse);
         
         if (ordonneeTampon<0.000000001 && ordonneeTampon >-0.000000001 && tab->donnees[compteurIntervalle-1]!=abscisse && tab->donnees[compteurIntervalle-1]!=abscisse-pas[compteurPas]) /* Autrement dit, =0 (donc on a trouvé une racine) mais avec les doubles, un == pourrait poser problème. Normalement on a environ droit à 15 chiffres significatifs.
                                                                                                                                                                                             Je vérifie aussi qu'on vient pas de déclarer l'abscisse précédente (ou actuelle car c'est possible aussi) comme racine, sinon c'est juste qu'on la détecte en double à tort.*/
@@ -146,7 +159,7 @@ void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double a
         compteurPas = compteurPas + 1;
     }
     
-    if (compteurIntervalle != 6) //On part de 0, si on trouve un changement on incrémente 2 fois de 1. Donc compteurIntervalle = 2 avec les cases 0 et 1 remplies. Même raisonnement jusqu'à 6.
+    if (compteurIntervalle != 6 && compteurIntervalle > 0) //On part de 0, si on trouve un changement on incrémente 2 fois de 1. Donc compteurIntervalle = 2 avec les cases 0 et 1 remplies. Même raisonnement jusqu'à 6.
     {
         double *NouvTableauAbscisses = (double*)realloc(tab->donnees,compteurIntervalle * sizeof(double)); // Donc si on a pas trouvé 3 racines, on renvoie un tableau qui contient le bon nombre de racines seulement.
         if (NouvTableauAbscisses != NULL) 
@@ -154,6 +167,21 @@ void PointsDepartNewton(Tableau *tab, double borneInf, double borneSup, double a
             tab->donnees = NouvTableauAbscisses; //Pas besoin de free quoique ce soit. Realloc a déjà libéré l'ancienne mémoire. Je crois que NouvTableauAbscisse demeure valable et est indispendable. En revanche, il n'y a bien qu'un seul espace mémoire d'alloué.
             tab->taille = compteurIntervalle;
             printf("Reallocation de la mémoire\n");
+        }
+        else{
+            printf("Erreur de réallocation mémoire : tableau de 6 cases --> tableau de %d cases\n",compteurIntervalle);
+        }
+    }
+    else if (compteurIntervalle == 0) // Si pas de racine dans l'intervalle
+    {
+        double *NouvTableauAbscisses = (double*)realloc(tab->donnees,2 * sizeof(double)); // Donc si on a pas trouvé 3 racines, on renvoie un tableau qui contient le bon nombre de racines seulement.
+        if (NouvTableauAbscisses != NULL) 
+        {
+            tab->donnees = NouvTableauAbscisses; //Pas besoin de free quoique ce soit. Realloc a déjà libéré l'ancienne mémoire. Je crois que NouvTableauAbscisse demeure valable et est indispendable. En revanche, il n'y a bien qu'un seul espace mémoire d'alloué.
+            tab->taille = 2;
+            tab->donnees[0] = -1;
+            tab->donnees[1] = -1;
+            printf("PAS DE RACINE SUR INTERVALLE ABSCISSES -> Reallocation de la mémoire -> intervalle = [-1;-1] \n");
         }
         else{
             printf("Erreur de réallocation mémoire : tableau de 6 cases --> tableau de %d cases\n",compteurIntervalle);
@@ -169,4 +197,103 @@ void instancierTableau(Tableau *tab, int taille){   // Cette fonction permet la 
         printf("Erreur de malloc : Structure tableau, allocation échouée");
         tab->taille = 0;
     }
+}
+
+double trouveQZB(double Z){
+    double Q = (Z+(1+sqrt(2))*B)/(Z+(1-sqrt(2))*B);
+    return Q;
+}
+
+double trouvePhi(double Z){
+    double phi = exp(Z-1-log(Z-B)-((A*sqrt(2)/(4*B))*log(Q))); 
+    return phi;
+}
+
+void NewtonRaphson(Tableau *tabResult, Tableau *tabIntervalles, double ecartZero){
+    int maxBoucle = 1000;
+    int i, compteurIntervalles;
+    double x0, x1, dfx0, mini, maxi;
+    
+    if (tabIntervalles->donnees[0]==-1 && tabIntervalles->donnees[1]==-1)
+    {
+        printf("(!) pas de racine sur l'intervalle -> racine = -1000");
+        tabResult->donnees[0] = -1000;
+        return;
+    }
+    
+    compteurIntervalles = 0;
+    tabResult->taille = 0;
+    for (i = 0; i < tabIntervalles->taille; i=i+2)
+    {   
+        x0 = tabIntervalles->donnees[i];
+        dfx0 = derivePR(x0);
+        if (dfx0 < 0.000000001 && dfx0 > -0.000000001)
+        {
+            printf("Tangente horizontale, x0 = x0 - 0.1");
+            x0 = x0 - 0.1;
+        }
+        
+        x1=0.1; // Simplement pour rentrer dans la boucle
+        while (fabs(PR(x1))>ecartZero && i<maxBoucle)
+        {
+            x1 = x0 - (PR(x0)/derivePR(x0));
+            x0 = x1;
+        }
+        tabResult->donnees[tabResult->taille]=x1;
+        tabResult->taille++;
+    }
+    mini = valeurMin(tabResult);
+    maxi = valeurMax(tabResult);
+    tabResult->donnees[0] = mini;
+    tabResult->donnees[1] = maxi;
+    double *NouvTab = (double*)realloc(tabResult->donnees,2 * sizeof(double)); 
+    if (NouvTab != NULL) 
+    {
+        tabResult->donnees = NouvTab; //Pas besoin de free quoique ce soit. Realloc a déjà libéré l'ancienne mémoire. Je crois que NouvTableauAbscisse demeure valable et est indispendable. En revanche, il n'y a bien qu'un seul espace mémoire d'alloué.
+        tabResult->taille = 2;
+        printf("Reallocation de la mémoire\n");
+    }
+    else{
+        printf("Erreur de réallocation mémoire fonction NewtonRaphton");
+    }
+}
+
+double valeurMax(Tableau *tab){
+    int i;
+    double valMax;
+    if (tab->taille==0)
+    {
+        printf("erreur : tableau de taille nulle. valeurMax = -1000");
+        return -1000;
+    }
+    
+    valMax = tab->donnees[0];
+    for (i = 0; i < tab->taille; i++)
+    {
+        if (tab->donnees[i]>valMax)
+        {
+            valMax = tab->donnees[i];
+        }
+    }
+    return valMax;
+}
+
+double valeurMin(Tableau *tab){
+    int i;
+    double valMin;
+    if (tab->taille==0)
+    {
+        printf("erreur : tableau de taille nulle. valeurMax = -1000");
+        return -1000;
+    }
+    
+    valMin = tab->donnees[0];
+    for (i = 0; i < tab->taille; i++)
+    {
+        if (tab->donnees[i]<valMin)
+        {
+            valMin = tab->donnees[i];
+        }
+    }
+    return valMin;
 }
